3ea9ad9f1cfe3eda9cec3606e2423a2b5a979d57
ShifaPatel6 <shifaapatel06>
2024-07-26 17:59:00 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import user from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res)=>{
+    const { username, email,password }= req.body;
+    const hashpass= bcryptjs.hashSync(password , 10);
+    const newUser= new user({ username , email , password: hashpass });
+    try{
+    await newUser.save();
+    res.status(201).json('User created sucessfully');
+    }
+    catch (error){
+        res.status(500).json(error);
+    }
+
+};
--- api/index.js
+++ api/index.js
@@ -1,20 +1,24 @@
 import express from 'express';
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'; 
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('Connect to mongodb!!!!!!');
+  console.log('connected to mongodb');
 })
 .catch((err)=>{
-    console.log(err);
-});
+  console.log(err);
+})
 
 const app = express();
 
-app.listen(3000, () =>{
-    console.log("Server is running on port 3000!!!");   
-});
+app.use(express.json());
 
-app.use('/api/user',userRouter);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!!!');
+  });
+
+  app.use('/api/user', userRouter); 
+  app.use('/api/auth', authRouter);
