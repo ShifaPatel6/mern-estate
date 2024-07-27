091bf400d2fc19676abce62d79b16352630551ac
ShifaPatel6 <shifaapatel06>
2024-07-27 11:28:25 +0530

    created a middlware and function for possible error.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -10,7 +10,7 @@ export const signup = async (req, res)=>{
     res.status(201).json('User created sucessfully');
     }
     catch (error){
-        res.status(500).json(error);
+        next(error);
     }
 
 };
