2a49b89a3adbdef9812fb0c7facef3440562059b
ShifaPatel6 <shifaapatel06>
2024-07-29 12:22:15 +0530

    created sign in API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import user from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import Jwt from 'jsonwebtoken';
 
 export const signup = async (req, res)=>{
     const { username, email,password }= req.body;
@@ -12,5 +14,23 @@ export const signup = async (req, res)=>{
     catch (error){
         next(error);
     }
+    
+};
+export const signin = async(req, res, next ) =>{
+    const{ email, password } = req.body;
 
+    try{
+        const validuser= await user.findOne({email});
+        if (!validuser) return next(errorHandler(404 ,'User not found'));
+        const validPassword = bcryptjs.compareSync(password , validuser.password);
+        if (!validPassword) return next(errorHandler(401, 'Invalid password'));
+        const token= Jwt.sign({id: validuser._id}, process.env.JWT_SECRET);
+        const {password: pass, ...rest}= validuser._doc;
+        res.cookie('access token', token, {httpOnly:true}).status(200).json(rest);  
+
+    }
+    catch (error){
+    next (error);
+        
+    }
 };
